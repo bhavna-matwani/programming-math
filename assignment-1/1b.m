16e77ac17fdb4bf089d968d36c3768706c89dd65
Bhavna Matwani <53350565+bhavna-matwani@users.noreply.github.com>
2022-09-30 00:39:21 -0400

    Added 1(b)

---
+++ assignment-1/1b.m
@@ -0,0 +1,30 @@
+function [L] = EulerSieve2(N)
+L=ones(1,N);
+L(1)=0;
+P=2;
+i=1;
+while (P^2)<=N
+    L1=zeros(1,N);
+    L2=zeros(1,N);
+    for i = 1:N
+       if(L(i)==true && i>=P && i<=N/P)
+        L1(i)=true;
+       end
+    end
+    for i = 1:N
+        if(L1(i)==true && P*i<=N)
+            L2(P*i)=true;
+        end
+    end
+    for i=1:N
+        if(L2(i)==true)
+            L(i)=false;
+        end
+    end
+    for i=P+1:Ns
+        if(L(i)==1)
+            P=i;
+            break
+        end
+    end
+end
