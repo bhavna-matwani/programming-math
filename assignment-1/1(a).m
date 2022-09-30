e2a2e3db2bcacd477dc953131480f000546ab689
Bhavna Matwani <53350565+bhavna-matwani@users.noreply.github.com>
2022-09-30 00:40:00 -0400

    Created 1(a)

---
+++ assignment-1/1(a).m
@@ -0,0 +1,17 @@
+function [L] = EulerSieve1(N)
+L=2:N;
+P=2;
+i=1;
+while (P^2)<=N
+    %L1=L;
+    if L(i)>=P && L(i)<=N/P
+        L1(i)=L(i);
+        i=i+1;
+    end
+    L2=P*L1;
+    L=setdiff(L,L2);
+    P=P+1;
+end
+end
+
+  
