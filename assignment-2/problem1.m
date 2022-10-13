ee2f31a4f34ccb7a4b17e7e5ea0d697031be2c07
Bhavna Matwani <53350565+bhavna-matwani@users.noreply.github.com>
2022-10-13 17:13:03 -0400

    Update and rename 1.m to problem1.m

--- assignment-2/problem1.m
+++ assignment-2/problem1.m
@@ -20,34 +20,30 @@ function [Found,P]=InvertMidpoints(Q)
         C(cnt,cnt+1)=0.5; 
         cnt=cnt+1;
     end
-    C
     
     if(rem(rowx_size,2))
-        C
-        P(:,1)=C\Q(:,1)
-        P(:,2)=C\Q(:,2)
+        P(:,1)=C\Q(:,1);
+        P(:,2)=C\Q(:,2);
     else
         flag=0;
         i=0;
         while(i<N)
             d=C;
-            d(N-i,:)=[]
+            d(N-i,:)=[];
             t=Q;
             t(N-i,:)=[];
             P(:,1)=d\t(:,1);
             P(:,2)=d\t(:,2);
-            P
             Q1(:,1)=C*P(:,1);
             Q1(:,2)=C*P(:,2);
-            Q1
             j=1;
             while(j<=N)
-                ex=abs(Q(j,1)-Q1(j,1))/Q(j,1)
-                ey=abs(Q(j,2)-Q1(j,2))/Q(j,2)
+                ex=abs(Q(j,1)-Q1(j,1))/Q(j,1);
+                ey=abs(Q(j,2)-Q1(j,2))/Q(j,2);
                 if(ex>10^(-8)| ey>10^(-8))
-                    Found=false
-                    P=zeros(N,2)
-                    flag=1
+                    Found=false;
+                    P=zeros(N,2);
+                    flag=1;
                     break;
                 end
                 j=j+1;
@@ -59,4 +55,4 @@ function [Found,P]=InvertMidpoints(Q)
         end   
         
     end
-end
+endv
