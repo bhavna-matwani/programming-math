208c7f1cfccb393c8a2ab5a6b35de70ba690de79
Bhavna Matwani <53350565+bhavna-matwani@users.noreply.github.com>
2022-10-12 22:19:09 -0400

    Create 2c.m

---
+++ assignment-2/2c.m
@@ -0,0 +1,44 @@
+[~,cols] = size(PB);
+[~,n] = size(PA);
+TF = zeros(2,n);
+for i = 1:n
+    TF(:,i)=TotalForce(PA(:,i),PB,Qb);
+end
+
+C = FindCharges(PA,PB,TF)
+
+function F=ForceMatrix(PA,PB)
+[~,cols] = size(PB);
+F = zeros(2,cols);
+    for i = 1:cols
+        pb = PB(:,i);
+        F(:,i) = (PA - pb)/(norm(PA-pb))^3;
+    end
+end
+
+%from 2b
+function TF=TotalForce(PA,PB,Qb)
+[~,cols] = size(PB);
+F = zeros(2,cols);
+    for i = 1:cols
+        pb = PB(:,i);
+        if(PA==pb)
+            disp("The Code Should Not Work");
+            break;
+        end
+        F(:,i) = (PA - pb)/(norm(PA-pb))^3;
+    end
+    TF = F*Qb;
+end
+
+function C=FindCharges(PA,PB,TF)
+A = ForceMatrix(PA(:,1),PB);
+B = TF(:,1);
+[~,n] = size(PA);
+for i = 2:n
+    F=ForceMatrix(PA(:,i),PB);
+    A = [A;F];
+    B = [B;TF(:,2)];
+end
+    C = A\B;
+end
